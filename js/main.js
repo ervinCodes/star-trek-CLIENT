0a554311d9fb7d38d80bd7eb5d5f693d1a016866
Ervin Natareno <ean1207@gmail.com>
2022-08-12 19:39:05 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+document.querySelector("button").addEventListener("click", apiRequest);
+
+async function apiRequest() {
+  const alienName = document.querySelector("input").value;
+  try {
+    const response = await fetch(
+      `https://star-trek-first-gen-aliens.herokuapp.com/api/${alienName}`
+    );
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
