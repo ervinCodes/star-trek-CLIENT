c4b3b823a6d6dc5437ec48e955e9535fc0a09888
Ervin Natareno <ean1207@gmail.com>
2022-08-13 12:20:42 -0700

    Styles and Functionality

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-document.querySelector("button").addEventListener("click", apiRequest);
+document.querySelector("#getButton").addEventListener("click", apiRequest);
 
 async function apiRequest() {
   const alienName = document.querySelector("input").value;
@@ -8,7 +8,38 @@ async function apiRequest() {
     );
     const data = await response.json();
     console.log(data);
+
+    document.getElementById("alienName").innerText = data.speciesName;
+    document.getElementById("alienWorld").innerText = data.homeworld;
+    document.getElementById("alienFeatures").innerText = data.features;
+    document.getElementById("alienFacts").innerText = data.interestingFact;
+    document.getElementById("alienExamples").innerText = data.notableExamples;
+
+    document.getElementById("alienImage").src = data.image;
+    document.getElementById("alienCaption").innerText = data.speciesName;
   } catch (error) {
     console.log(error);
   }
 }
+
+// On Load function
+window.onload = function () {
+  const url = `https://star-trek-first-gen-aliens.herokuapp.com/api/vulcans`;
+
+  fetch(url)
+    .then((res) => res.json()) // parse response as JSON
+    .then((data) => {
+      console.log(data);
+      document.getElementById("alienName").innerText = data.speciesName;
+      document.getElementById("alienWorld").innerText = data.homeworld;
+      document.getElementById("alienFeatures").innerText = data.features;
+      document.getElementById("alienFacts").innerText = data.interestingFact;
+      document.getElementById("alienExamples").innerText = data.notableExamples;
+
+      document.getElementById("alienImage").src = data.image;
+      document.getElementById("alienCaption").innerText = data.speciesName;
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
+};
